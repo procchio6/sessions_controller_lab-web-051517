24ef4fcff484d5b065f5e4bdfaeb79abde3d5bca
Pat Rocchio <procchio6@gmail.com>
2017-06-13 13:01:06 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'application#hello'
-  get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
 
